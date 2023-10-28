57690577b52baa22933ece16453966b0a04844bd
Aishu0609 <aishwaryal.xworkz@gmail.com>
2023-10-28 14:22:21 +0530

    Hello world

---
+++ Airpods.java
@@ -0,0 +1,20 @@
+class Airpods{
+	static double weight=400;
+	static String chargecase="Ctype";
+	static String brand="boult";
+	static String connectivity="bluetooth";
+	static double cost=1000.00;
+	
+	static void AirpodsInfo(){
+		System.out.println("Information of Airpods Started");
+		System.out.println("Airpods specification");
+		System.out.println("Airpods weight:"+weight);
+		System.out.println("Airpods chargecase"+chargecase);
+		System.out.println("Airpods brand:"+brand);
+		System.out.println("Airpods connectivity:"+connectivity);
+		System.out.println("Airpods cost:"+cost);
+		System.out.println("Information of Airpods Ended");
+	}
+}
+		
+		
