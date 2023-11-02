c9d65e3cc39a33cbc5a8e7e68cbb6ed7e2df5276
Aishu0609 <aishwaryal.xworkz@gmail.com>
2023-11-03 00:51:08 +0530

    Contructor

---
+++ PlacementStarter.java
@@ -0,0 +1,57 @@
+class PlacementStarter{
+    public static void main(String[] args){
+
+        System.out.println("Starting main in Placement");
+
+        Placement placement=new Placement("Aishu",150,"MCA",9875451244L,"UBDT",200,"IBM","Praju",600,8854612789L,114546,"Davangere");
+        placement.displayInfo();
+		System.out.println("\n");
+
+        Placement placement1=new Placement("Sujana",200,"MCA",9375638291L,"BIET",117,"Accenture","Manja",400,84958503329L,2435465,"durga");
+        placement1.displayInfo();
+		System.out.println("\n");
+
+
+        Placement placement2=new Placement("Megha",165,"BE",8392016371L,"Jain",110,"Cognizent","Mohan",300,9875432145L,342156,"Hubli");
+        placement2.displayInfo();
+		System.out.println("\n");
+
+        Placement placement3=new Placement("Bhavana",165,"ITI",9879063535L,"AVK",102,"Zoho","Kavya",100,936475849L,6374849,"Manglore");
+        placement3.displayInfo();
+		System.out.println("\n");
+
+        Placement placement4=new Placement("Swathi",150,"ECE",81920425167L,"SPC",100,"Amazon","Naveen",250,8493028271L,9574830,"Dharwad");
+        placement4.displayInfo();
+		System.out.println("\n");
+
+        Placement placement5=new Placement("Jyothi",130,"EEE",8293745292L,"Alvas",250,"Flipkart","Vedavathi",350,9373628291L,8372829,"Mysore");
+        placement5.displayInfo();
+		System.out.println("\n");
+
+        Placement placement6=new Placement("Ammu",250,"CS",7384940382L,"Rnsit",100,"Cisco","Vasu",550,8364859402L,9287292,"Bellary");
+        placement6.displayInfo();
+		System.out.println("\n");
+
+        Placement placement7=new Placement("Niharika",150,"Course",93627282839L,"Infotech",400,"TCS","Chaitrika",100,9282651727L,464546,"Haveri");
+        placement7.displayInfo();
+		System.out.println("\n");
+
+        Placement placement8=new Placement("Shree",450,"SJVT",7890526271L,"Wipro",100,"SoftwareTrainee","Ashwini",120,9273629292L,9372762,"Gadag");
+        placement8.displayInfo();
+		System.out.println("\n");
+
+        Placement placement9=new Placement("Deekshita",120,"MCA",9373528291L,"UBDT",220,"Genpact","Varsha",300,7483920214L,7389201,"Chelekere");
+        placement9.displayInfo();
+		System.out.println("\n");
+
+        Placement placement10=new Placement("Deepu",250,"EEE",9473527289L,"UBDT",200,"Relaince","Sheetal",203,92736482920L,8394039,"Shimogga");
+        placement10.displayInfo();
+		System.out.println("\n");
+
+        Placement placement11=new Placement("Reetu",140,"MCA",7382923930L,"UBDT",240,"Smallday","Preeti",290,93722100293L,9374832,"Pune");
+        placement11.displayInfo();
+		System.out.println("\n");
+
+        System.out.println("Ending main in placement");
+    }
+}
