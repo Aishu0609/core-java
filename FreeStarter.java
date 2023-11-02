886b865c01febc0103c5455ce556387cfd2ee4eb
Aishu0609 <aishwaryal.xworkz@gmail.com>
2023-11-02 23:22:47 +0530

    Contructor

---
+++ FreeStarter.java
@@ -0,0 +1,59 @@
+class FreeStarter{
+	public static void main(String[] args){
+		
+		System.out.println("Starting main in FreeStarter");
+		
+		Free free=new Free("Watch",true,"Vocher");
+		free.displayInfo();
+		System.out.println("\n");
+		
+		
+		Free free1=new Free("Perfume",false,"offer");
+		free1.displayInfo();
+		System.out.println("\n");
+		
+		Free free2=new Free("Cosmetics",false,"LuckyCoupon");
+		free2.displayInfo();
+		System.out.println("\n");
+		
+		Free free3=new Free("Grocery",true,"Draw");
+		free3.displayInfo();
+		System.out.println("\n");
+		
+		Free free4=new Free("Games",false,"Coupon");
+		free4.displayInfo();
+		System.out.println("\n");
+		
+		Free free5=new Free("Mobile",true,"Exchangeoffer");
+		free5.displayInfo();
+		System.out.println("\n");
+		
+		Free free6=new Free("Laptop",true,"Vocher");
+		free6.displayInfo();
+		System.out.println("\n");
+		
+		Free free7=new Free("Dress",true,"LuckyVocher");
+		free7.displayInfo();
+		System.out.println("\n");
+		
+		Free free8=new Free("Toys",false,"Coupon");
+		free8.displayInfo();
+		System.out.println("\n");
+		
+		Free free9=new Free("Lights",true,"LuckyDraw");
+		free9.displayInfo();
+		System.out.println("\n");
+		
+		Free free10=new Free("cables",false,"Festivaloffer");
+		free10.displayInfo();
+		System.out.println("\n");
+		
+		Free free11=new Free("wood",true,"Vocher");
+		free11.displayInfo();
+		System.out.println("\n");
+		
+		System.out.println("Ending main in FreeStarter");
+	}
+}
+		
+		
